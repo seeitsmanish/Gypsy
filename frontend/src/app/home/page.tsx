d7d17c8a918bd26c3e5857bc965ca90cab92f471
See-Its-Manish <seeitsmanish@gmail.com>
2023-05-08 21:56:08 +0530

    Added Search AutoComplete

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -4,6 +4,9 @@ import { useState, useEffect } from "react";
 
 import {
   Box,
+  Menu,
+  MenuButton,
+  MenuList,
   Stack,
   Card,
   Text,
@@ -24,7 +27,18 @@ import {
   AccordionButton,
   AccordionPanel,
   AccordionIcon,
+  Flex,
+  FormControl,
+  FormLabel,
+  FormHelperText,
 } from "@chakra-ui/react";
+
+// import { CUIAutoComplete } from "chakra-ui-autocomplete";
+
+// For Search ReactSearchAutocomplete
+import { ReactSearchAutocomplete } from 'react-search-autocomplete'
+
+
 
 import FeatherIcon from "feather-icons-react";
 import Maps from "../../../components/Maps";
@@ -35,21 +49,103 @@ import map from "./map.png";
 import styles from "./page.module.css";
 
 export default function Home() {
-  const [minimised, setMinimised] = useState(false);
+  const [source, setSource] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [dest, setDest] = useState("");
+  let [suggestions, setSuggestions] = useState(["NIET", "GNIOT", "IILM"]);
+  
+  // const handleInputChange = (event) => {
+  //   const inputValue = event.target.value;
+  //   setSource(inputValue);
+  //   // fetch suggestions based on input value and update suggestions state
+  //   setSuggestions(["suggestion 1", "suggestion 2", "suggestion 3"]);
+  //   setIsOpen(true);
+  // };
 
-  function handleMinimise() {
-    setMinimised(!minimised);
+  // const handleMenuItemClick = () => {
+  //   setSuggestions("nIET");
+  //   setIsOpen(false);
+  // };
+
+  // const handleChangeSource = async (event) => {
+  //   setSource(event.target.value);
+  //     if (event.target.value.length >= 3) {
+  //       // setSuggestions([]);
+  //       const response = await fetch(`http://127.0.0.1:8000/api/search/?text=${event.target.value}&maxResults=5`);
+  //       const data = await response.json();
+  //       // setSuggestions(response);
+  //       // data.Results.forEach((result) => {
+  //       //   suggestions.push(result.Place.Label);
+  //       // });
+  //       const newList = [];
+  //       // console.log(data);
+        
+  //       for(let resultIndex = 0; resultIndex < data.Results.length; resultIndex++){
+  //         newList.push(data.Results[resultIndex].Place.Label);
+  //       }
+  //       console.log(newList);
+  //       setSuggestions(newList);
+  //     }
+  //     // console.log(suggestions);
+  // };
+
+  // const handleChangeDest = (event) => {
+  //   setDest(event.target.value);
+  //   console.log(dest);
+  // };
+
+  const [items, setItems] = useState([{}]);
+
+  const handleOnSearch = async (string, results) => {
+    // onSearch will have as the first callback parameter
+    // the string searched and for the second the results.
+
+    const response = await fetch(`http://127.0.0.1:8000/api/search/?text=${string}&maxResults=5`);
+    const data = await response.json();
+    const newList = [];
+    for(let resultIndex = 0; resultIndex < data.Results.length; resultIndex++){
+      newList.push({
+        id: resultIndex,
+        name : data.Results[resultIndex].Place.Label
+      });
+    }
+    setItems(newList);
+    console.log(newList);
+    console.log(string, results)
+  }
+
+  const handleOnHover = (result) => {
+    // the item hovered
+    console.log(result)
+  }
+
+  const handleOnSelect = (item) => {
+    // the item selected
+    console.log(item)
+  }
+
+  const handleOnFocus = () => {
+    console.log('Focused')
+  }
+
+  const formatResult = (item) => {
+    return (
+      <>
+        {/* <span style={{ display: 'block', textAlign: 'left' }}>id: {item.id}</span> */}
+        <span style={{ display: 'block', textAlign: 'left' }}>{item.name}</span>
+      </>
+    )
   }
 
   return (
     <div>
-      <div className={minimised ? styles.minimised : styles.sidebar}>
+      <div className={styles.sidebar}>
         <div className={styles.center}>
           <Image src={logo} alt="Logo" height={200} width={200} />
         </div>
 
-        <div className={minimised ? styles.minimised : styles.niet_hackathon}>
-          <Text fontSize="xs">NIET HACKATHON PREWIEW</Text>
+        <div className={styles.niet_hackathon}>
+          <Text fontSize="xs">NIET HACKATHON PREVIEW</Text>
         </div>
         <div className={styles.main_content}>
           <Stack spacing={2}>
@@ -67,26 +163,35 @@ export default function Home() {
                 <Tab _selected={{ color: "white", bg: "blue.600" }}>Foot</Tab>
               </TabList>
             </Tabs>
-            <InputGroup>
-              <InputLeftAddon
-                children={<FeatherIcon icon="arrow-up-right" />}
-              />
-              <Input
-                type="tel"
-                className={styles.input_bg}
-                placeholder="Source"
-                _placeholder={{ color: "#A0AEC0" }}
-              />
-            </InputGroup>
-            <InputGroup>
-              <InputLeftAddon children={<FeatherIcon icon="arrow-down" />} />
-              <Input
-                className={styles.input_bg}
-                type="tel"
-                placeholder="Destination"
-                _placeholder={{ color: "#A0AEC0" }}
-              />
-            </InputGroup>
+
+            <Stack spacing={2}>
+              <div className={styles.inputGrp}>
+                <FeatherIcon icon="arrow-up-right" />
+
+                {/* For Search AutoComplete */}
+                <div className="App">
+                  <header className="App-header">
+                    <div style={{ width: 400 }}>
+                      <ReactSearchAutocomplete
+                        items={items}
+                        onSearch={handleOnSearch}
+                        onHover={handleOnHover}
+                        onSelect={handleOnSelect}
+                        onFocus={handleOnFocus}
+                        autoFocus
+                        formatResult={formatResult}
+                      />
+                    </div>
+                  </header>
+                </div>
+
+              </div>
+
+              <div className={styles.inputGrp}>
+                <FeatherIcon icon="arrow-down" />
+
+              </div>
+            </Stack>
           </Stack>
 
           <Stack spacing={1}>
@@ -133,7 +238,7 @@ export default function Home() {
                     </AccordionButton>
                   </h2>
                   <AccordionPanel pb={4}>
-                    The average fuel is fucked bro.
+                    The average fuel consumption is lower than usual.
                   </AccordionPanel>
                 </AccordionItem>
 
@@ -149,9 +254,9 @@ export default function Home() {
                   <AccordionPanel pb={4}>
                     <Text>
                       <ul>
-                        <li>GB Road (Strip Club)</li>
+                        <li>NIET Greater Noida</li>
                         <li>Pandey's Home (Arcade Gaming)</li>
-                        <li>NIET College</li>
+                        <li>IILM College Greater Noida</li>
                       </ul>
                     </Text>
                   </AccordionPanel>
@@ -167,8 +272,9 @@ export default function Home() {
                     </AccordionButton>
                   </h2>
                   <AccordionPanel pb={4}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Facere omnis nesciunt provident, p
+                    Route between IILM College and NIET College is very good. No
+                    traffic is to be expected. Drive faster than normal to reach
+                    in optimal time.
                   </AccordionPanel>
                 </AccordionItem>
               </Accordion>
@@ -176,15 +282,6 @@ export default function Home() {
           </Card>
         </div>
       </div>
-      {minimised ? (
-        <button className={styles.minimise_btn} onClick={handleMinimise}>
-          <FeatherIcon icon="chevrons-down" />
-        </button>
-      ) : (
-        <button className={styles.minimise_btn} onClick={handleMinimise}>
-          <FeatherIcon icon="chevrons-up" />
-        </button>
-      )}
 
       {/* <Image
         className={styles.map}
