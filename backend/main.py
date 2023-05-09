f6cd7edd300a279bb67e01ac585afe3174a3c313
abhishak3 <abhishek.pandey.3237@gmail.com>
2023-05-09 13:59:07 +0530

    fixed cors error

--- backend/main.py
+++ backend/main.py
@@ -106,7 +106,7 @@ async def getRoute(route: RouteRequest):
     return listWithDistDurAndRoutes
 
 
-@app.post("/api/getRoutes")
+@app.post("/api/getRoutes/")
 async def getRoutes(route: RouteRequest):
     """
     Get multiple routes for different times.
@@ -138,7 +138,7 @@ async def getRoutes(route: RouteRequest):
     }
 
 
-@app.post("/api/getSafestPath")
+@app.post("/api/getSafestPath/")
 async def getSafestPath(route: RouteRequest):
     """
     Get Safest Path
